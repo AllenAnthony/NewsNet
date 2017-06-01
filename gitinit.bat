9f9b943ad51674eae6df0acb12c30b9725cf4902
Anthony <1875898003@qq.com>
2017-06-01 20:05:38 +0800

    :

--- gitinit.bat
+++
@@ -1,8 +0,0 @@
-call git init
-call git add .
-call git commit -m "first commit"
-call git remote add origin %1
-call git pull
-call git pull origin master
-call git pull --rebase origin master
-call git push -u origin master
